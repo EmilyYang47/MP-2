be446330c6abad6e947cf0e07b5bb6f2a0ba9be9
EmilyYang47 <eyang4@bu.edu>
2025-07-14 15:32:32 -0400

    initialize repository

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import IceAndFire from "./components/IceAndFire.tsx";
+import styled from "styled-components";
+import { useEffect, useState } from 'react'
+import {Character} from "./interfaces/Characters.ts";
+
+const ParentDiv=styled.div`
+    width: 80vw;
+    margin: auto;
+    border: 5px darkgoldenrod solid;
+`;
+
+export default function App() {
+  // useState Hook to store Data. 
+  const [data, setData] = useState<Character[]>([]); 
+
+  // useEffect Hook for error handling and re-rendering. 
+  useEffect(()=>{
+    async function fetchData(): Promise<void> {
+      const characters: Character[] = []; 
+
+      // iterate over each id from range [1,100], since the https://anapioficeandfire.com/api/characters/ only provides the first few characters 
+      for (let id = 1; id <= 100; id++) {
+        try {
+          const rawData = await fetch(`https://anapioficeandfire.com/api/characters/${id}`); 
+          const character = await rawData.json(); 
+          
+          // only load the characters with a name field 
+          if (character.name){ 
+            // Create a new character object that combines all fields from the fetched character with an additional id field. 
+            // "...character" is the fields from the fetched character, "id: id" is the id field.  
+            // Resource: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-1.html#object-spread
+            const characterObject : Character = {...character, id: id} 
+
+            // push the Character object into characters 
+            characters.push(characterObject); 
+          } 
+        } catch (e) { 
+          console.log(`Error fetching character ${id}: `+ e); 
+        } 
+      }
+
+      setData(characters); 
+    } 
+
+    fetchData()
+      .then(() => console.log("Data fetched successfully")) 
+      .catch((e: Error) => console.log("there was the error: " + e)); 
+  }, [data.length]); 
+
+  return (
+    <ParentDiv>
+      <IceAndFire data={data}/>
+    </ParentDiv>
+  )
+}
+
---
+++ src/components/IceAndFire.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components"; 
+import {Character} from "../interfaces/Characters.ts"; 
+
+const AllCharsDiv = styled.div`
+    display: flex; 
+    flex-flow: row wrap; 
+    justify-content: space-evenly; 
+    background-color: bisque; 
+`; 
+
+const SingleCharDiv = styled.div`
+    display: flex; 
+    flex-direction: column; 
+    justify-content: center; 
+    max-width: 30%;
+    padding: 2%;
+    margin: 1%; 
+    background-color: ${(props)=>(props.culture === "Westeros" ? 'darkorange' : (props.culture === "Valyrian" ? '#E3F1FF' 
+                                                                            : (props.culture === "Stormlands" ? '#9704b4ff' 
+                                                                            : (props.culture === "Ironborn" ? '#f3cad4ff' 
+                                                                            : (props.culture === "Andal" ? '#7c1100ff' 
+                                                                            : (props.culture === "Northmen" ? '#feea69ff' 
+                                                                            : 'black'))))))};
+    color: ${(props) => (props.culture === "Westeros" ? 'black' : (props.culture === "Valyrian" ? '#0061A2' 
+                                                                : (props.culture === "Stormlands" ? '#fbe1f7ff' 
+                                                                : (props.culture === "Ironborn" ? '#84001fff' 
+                                                                : (props.culture === "Andal" ? '#f5e2dfff' 
+                                                                : (props.culture === "Northmen" ? '#b82508ff' 
+                                                                : 'white'))))))}; 
+    
+    border: 3px darkred solid;
+    font: italic small-caps bold calc(2px + 1vw) Papyrus, fantasy;
+    text-align: center; 
+`
+
+export default function IceAndFire(props: {data:Character[]}){
+    return (
+        <AllCharsDiv>
+            {
+                props.data.map((char: Character) => 
+                    <SingleCharDiv key={char.id} culture={char.culture} >
+                        <h1>{char.name}</h1>
+                        <p>{char.gender}</p>
+                        <p>{char.culture ? char.culture : "No Culture"}</p>
+                    </SingleCharDiv> 
+                )
+            }
+        </AllCharsDiv>
+    )
+}
